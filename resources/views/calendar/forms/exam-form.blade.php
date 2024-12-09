69b0688205086b82fece9e5fc17cf57f62479a65
IgorCroitoru1 <igor.croitoru@student.usv.ro>
2024-12-09 23:41:22 +0200

    proposal-functionality

--- resources/views/calendar/forms/exam-form.blade.php
+++ resources/views/calendar/forms/exam-form.blade.php
@@ -56,7 +56,7 @@
     </div>
 
     <div class="col-span-2 sm:col-span-1">
-    <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deurata (min)</label>
+    <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Durata (min)</label>
     <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="120" required />
         <p id='durata-error-message'class="mt-2 text-sm text-red-600 dark:text-red-500 hidden">Durate trebuie sa fie cuprinsa intre 10 si 360 min</p>    
     </div>
