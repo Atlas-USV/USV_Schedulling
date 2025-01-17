c6a8154ed33b2d379912e220f5ce381d3ff13113
Catargiu Stefan <stefan.catargiu@webitfactory.io>
2025-01-17 19:43:21 +0200

    Dashboard Page fix and Exams page fix and revamp.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -145,12 +145,7 @@ class AuthController extends Controller
         $request->session()->regenerateToken();
 
         
-        // Cookie::queue(Cookie::forget('laravel_session'));
-        // Cookie::queue(Cookie::forget('XSRF-TOKEN'));
-<<<<<<< Updated upstream
+       
         return redirect('/');
-=======
-        return redirect('home');
->>>>>>> Stashed changes
     }
 }
